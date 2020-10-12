41c8c6aa9761b9ceacaaeba5cfda9898b1228d8b
Matthew Johannes C. Balubar <matthew.balubar@gmail.com>
2020-10-12 12:23:07 +0800

    v2020.0.0a1
    INITIAL COMMIT

---
+++ src/activity/output/MyFirstJavaProgram/MyFirstJavaProgram.java
@@ -0,0 +1,31 @@
+package activity.output.MyFirstJavaProgram;
+
+/**
+ *
+ * <h1> Scripts.MyFirstJavaProgram </h1>
+ * <h2> This program prints “ Hello World” on screen </h2>clear
+ *
+ * @author Matthew Johannes C. Balubar
+ * @version 2020.0.0a1
+ * @since 2020-12-10
+ *
+ * @moduleNumber Module #2
+ * @moduleWeek Week-2
+ * @activityNumber Activity #4
+ *
+ */
+
+public class MyFirstJavaProgram
+{
+    /**
+     * Start is called on the frame when a script is enabled just before any of the methods are called the first time.
+     * @param args - accepts a single argument of type String arra
+     */
+    public static void main(String args[])
+    {
+        // Prints "Hello World" in the console
+        System.out.print("Hello World!");
+
+        // Outputs "Hello World"
+    }
+}
